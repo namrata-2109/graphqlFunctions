5e734b9850e4cacc29ec6bae8388e26a1d7b111c
Swapnanil Saha <greatdreamblue4545@gmail.com>
2022-05-20 22:42:45 +0530

    1st commit

---
+++ src/gqlFunction/OrderTaskTable.js
@@ -0,0 +1,40 @@
+import { API } from 'aws-amplify';
+import * as mutations from '../graphql/mutations';
+import * as queries from '../graphql/queries';
+export const createTask = async (taskDetails) => {
+    try {
+        const addTask= await API.graphql({ query: mutations.createOrderTask, variables: { input: taskDetails } })
+        console.log("Task has been added", addTask.data.createOrderTask);
+    } catch (error) {
+        console.log("error in creating ", error);
+    }
+}
+
+export  const updateTask = async(user)=>{
+    try {
+      console.log("Get user to update ")
+        const updatedTask=await API.graphql({query:mutations.updateOrderTask,variables:{input: user}});
+        console.log("Updated Task is ",updatedTask.data.updateOrderTask);
+    }catch (error) {
+        console.log("Error in updating ",error);
+    }
+}
+
+export const deleteTask = async (Mail) => {
+    try {
+        const deletedTask = await API.graphql({ query: mutations.deleteOrderTask, variables: {input: Mail} })
+        console.log("Deleted Task is ", deletedTask.data.deleteOrderTask);
+    } catch (error) {
+        console.log("Error in deleting ", error);
+    }
+} 
+
+export const getTaskbyId = async(userEmail) => {
+    try {
+            const getTask = await API.graphql({ query: queries.getOrderTask, variables: {TaskID: userEmail.TaskID }});
+            console.log('Task to be read is', getTask.data.getOrderTask)
+    }
+    catch(error) {
+           console.log("Error in gettask",error);
+          }
+}
--- src/gqlFunction/UserTable.js
+++ src/gqlFunction/UserTable.js
@@ -2,11 +2,17 @@
 import { API } from 'aws-amplify';
 import * as mutations from '../graphql/mutations'
 import * as queries from '../graphql/queries';
+import {emailValidation,phoneValidation} from '../InputTest';
 
 // email, name, isAdmin, phone, superviserEmail, isApproved, isEmailApproved, isPhoneVerified, isGooleSignIn,isFacebookSignIn ,isGeneralAuthSignIn
 
 // create instance of userDetails in App.js
  export const createNewUser = async (userDetails) => {
+    if(userDetails.email=="" || userDetails.name=="" || typeof userDetails.isAdmin!= "boolean" || userDetails.phone=="" || userDetails.superwiserEmail=="" || typeof userDetails.isApproved!="boolean" || typeof userDetails.isEmailApproved!="boolean" || typeof userDetails.isPhoneVerified!="boolean" || typeof userDetails.isGooleSignIn!="boolean" || typeof userDetails.isFacebookSignIn!="boolean" || typeof userDetails.isGeneralAuthSignIn!="boolean"){
+        throw new Error("null value not allowed")
+        }
+    emailValidation(userDetails.email)
+    phoneValidation(userDetails.phone)
      try {
          const addUser = await API.graphql({ query: mutations.createUser, variables: { input: userDetails } })
          console.log("User has been added", addUser.data.createUser);
@@ -17,6 +23,7 @@ import * as queries from '../graphql/queries';
 
 //create instance of Mail in App.js
 export const deleteUserByMail = async (Mail) => {
+    emailValidation(Mail.email)
     try {
         const deletedUser = await API.graphql({ query: mutations.deleteUser, variables: {input: Mail} })
         console.log("Deleted User is ", deletedUser.data.deleteUser);
@@ -26,19 +33,42 @@ export const deleteUserByMail = async (Mail) => {
 }
 
 //create instance of supEmail in App.js
-export const deleteUserBySupMail = async (supEmail) => {
+export const deleteUserBySupMail = async (userSupEmail) => {
+    emailValidation(userSupEmail.superwiserEmail)
+
     try {
-     const deleteTheUser = await API.graphql({ query: mutations.deleteUser, variables: { input: supEmail} });
-     console.log("Deleted User is ", deleteTheUser.data.deleteUser);
-         } catch (error) {
-             console.log("Error in deleting ", error);
-         }
+        const userSupData = await API.graphql({ query: queries.userBySuperWisedID, variables: {superwiserEmail: userSupEmail } });
+        console.log("User details by supervisor email", userSupData.data.userBySuperWisedID)
+        const listItems = userSupData.data.userBySuperWisedID.items;
+        console.log(listItems.length)
+      for(var i=0 ; i<listItems.length ; i++)
+      {
+          console.log(i)
+          console.log(listItems[i].email)
+          const deleteList = {
+              email: listItems[i].email,                  
+              _version: listItems[i]._version
+            }
+          const deleteTheUser = await API.graphql({ query: mutations.deleteUser, variables: { input: deleteList} });
+          console.log("Deleted User is ", deleteTheUser.data.deleteUser);
       }
+     //   var len =  arrr.length;
+        
+    }
+    catch (error){
+        console.log("Error in getUser", error);
+    }
+}
+
 
 
 export const getUserByEmail = async(userEmail) => {
+    emailValidation(userEmail)
+
     try {
             const userData = await API.graphql({ query: queries.getUser, variables: {email: userEmail }});
+            const x = userData.data.getUser;
+            console.log(x)
             console.log("User details by email", userData.data.getUser)
     }
     catch {
@@ -48,6 +78,8 @@ export const getUserByEmail = async(userEmail) => {
 
 // create instance of userSupEmail in App.js
       export const getUserBySupMail = async (userSupEmail) => {
+        emailValidation(userSupEmail)
+
           try {
               const userSupData = await API.graphql({ query: queries.userBySuperWisedID, variables: {superwiserEmail: userSupEmail } });
               console.log("User details by supervisor email", userSupData.data.userBySuperWisedID)
@@ -59,6 +91,7 @@ export const getUserByEmail = async(userEmail) => {
 
 // create instance of updatedData in App.js
      export  const updateUserInfo = async(user)=>{
+        emailValidation(user.email)
       try {
         console.log("Get user to update ")
           const getUpdateUser = await API.graphql({query:queries.getUser, variables:{email: user.email}})
